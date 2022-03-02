8433ca6bdb98095c631a1bc6cce1bf75fce82884
Rafael Ramos <arkanjo.ms@gmail.com>
2022-03-02 17:32:14 -0300

    fix localstack config

--- 3-desafio/1-aplicacao-escolar/financial/main.go
+++ 3-desafio/1-aplicacao-escolar/financial/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"financial/app/consumer"
 	"financial/app/controller"
+	"github.com/dantasrafael/DojoGo/tree/master/3-desafio/starters/messaging"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -11,5 +13,9 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/invoices", controller.NewInvoiceController().Find)
 
+	sess := messaging.CreateLocalstackSession()
+	consumer.StartSchoolEnrollmentConsumer(sess)
+
+	log.Println("starting application on port 8181")
 	log.Fatal(http.ListenAndServe(":8181", router))
 }
