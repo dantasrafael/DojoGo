d7488f5a05daf75c87d2452df9f1d1a0253bf3ed
Rafael Dantas <dantasrafael@yahoo.com.br>
2022-03-03 15:46:58 -0300

    sqs starter

--- 3-desafio/starters/messaging/sqs.go
+++ 3-desafio/starters/messaging/sqs.go
@@ -1,12 +1,54 @@
 package messaging
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/sqs"
+	"encoding/json"
 	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
-func ReadMessages(svc *sqs.SQS, queueResult *sqs.GetQueueUrlOutput) (*sqs.ReceiveMessageOutput, error) {
+func CreateConsumer(sess *session.Session, queueName string) chan *ProviderMessage {
+	ch := make(chan *ProviderMessage, 1)
+
+	svc := sqs.New(sess)
+	queueResult, err := svc.GetQueueUrl(&sqs.GetQueueUrlInput{QueueName: aws.String(queueName)})
+	if err != nil {
+		log.Printf("could not connect to queue %s: %v\n", queueName, err)
+		os.Exit(1)
+	}
+
+	go func() {
+		for {
+			msgs, err := readMessages(svc, queueResult)
+			if err != nil {
+				log.Printf("could not receive message: %v\n", err)
+				continue
+			}
+			if len(msgs.Messages) > 0 {
+				msg := msgs.Messages[0]
+				var n ProviderMessage
+				log.Println(*msg.Body)
+
+				err = json.Unmarshal([]byte(*msg.Body), &n)
+				if err != nil {
+					log.Printf("Could not unmarshal message body [%s]: %v\n", *msg.Body, err)
+				} else {
+					ch <- &n
+					removeMessageFromQueue(svc, queueResult, msg)
+				}
+			} else {
+				log.Println("No messages in queue..")
+			}
+		}
+	}()
+
+	return ch
+}
+
+func readMessages(svc *sqs.SQS, queueResult *sqs.GetQueueUrlOutput) (*sqs.ReceiveMessageOutput, error) {
 	var msgs, err = svc.ReceiveMessage(&sqs.ReceiveMessageInput{
 		QueueUrl:              queueResult.QueueUrl,
 		MaxNumberOfMessages:   aws.Int64(1),
@@ -16,7 +58,7 @@ func ReadMessages(svc *sqs.SQS, queueResult *sqs.GetQueueUrlOutput) (*sqs.Receiv
 	return msgs, err
 }
 
-func RemoveMessageFromQueue(svc *sqs.SQS, queueResult *sqs.GetQueueUrlOutput, msg *sqs.Message) {
+func removeMessageFromQueue(svc *sqs.SQS, queueResult *sqs.GetQueueUrlOutput, msg *sqs.Message) {
 	_, err := svc.DeleteMessage(&sqs.DeleteMessageInput{
 		QueueUrl:      queueResult.QueueUrl,
 		ReceiptHandle: msg.ReceiptHandle,
