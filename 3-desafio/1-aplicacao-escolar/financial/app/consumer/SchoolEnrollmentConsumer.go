6b7a8621e478f7eca5deaa35f76452fbe6c82ffb
Rafael Dantas <dantasrafael@yahoo.com.br>
2022-03-03 15:44:36 -0300

    create consumer starter

--- 3-desafio/1-aplicacao-escolar/financial/app/consumer/SchoolEnrollmentConsumer.go
+++ 3-desafio/1-aplicacao-escolar/financial/app/consumer/SchoolEnrollmentConsumer.go
@@ -2,22 +2,19 @@ package consumer
 
 import (
 	"context"
-	"encoding/json"
 	"financial/app/consumer/model"
 	"financial/domain/entity"
 	"financial/domain/usecase"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/sqs"
-	"github.com/dantasrafael/DojoGo/tree/master/3-desafio/starters/messaging"
 	"log"
-	"os"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/dantasrafael/DojoGo/tree/master/3-desafio/starters/messaging"
 )
 
 const schoolEnrollmentQueueName = "SCHOOL_ENROLLMENT_FINANCIAL"
 
 func StartSchoolEnrollmentConsumer(sess *session.Session) {
-	ch := createConsumer(sess)
+	ch := messaging.CreateConsumer(sess, schoolEnrollmentQueueName)
 	uc := usecase.NewRegisterAccount()
 
 	for {
@@ -41,41 +38,4 @@ func StartSchoolEnrollmentConsumer(sess *session.Session) {
 			}
 		}
 	}
-}
-
-func createConsumer(sess *session.Session) chan *messaging.ProviderMessage {
-	ch := make(chan *messaging.ProviderMessage, 1)
-
-	svc := sqs.New(sess)
-	queueResult, err := svc.GetQueueUrl(&sqs.GetQueueUrlInput{QueueName: aws.String(schoolEnrollmentQueueName)})
-	if err != nil {
-		log.Printf("could not connect to queue %s: %v\n", schoolEnrollmentQueueName, err)
-		os.Exit(1)
-	}
-
-	go func() {
-		for {
-			msgs, err := messaging.ReadMessages(svc, queueResult)
-			if err != nil {
-				log.Printf("could not receive message: %v\n", err)
-				continue
-			}
-			if len(msgs.Messages) > 0 {
-				msg := msgs.Messages[0]
-				var n messaging.ProviderMessage
-				log.Println(*msg.Body)
-				err = json.Unmarshal([]byte(*msg.Body), &n)
-				if err != nil {
-					log.Printf("Could not unmarshal message body [%s]: %v\n", *msg.Body, err)
-				} else {
-					ch <- &n
-					messaging.RemoveMessageFromQueue(svc, queueResult, msg)
-				}
-			} else {
-				log.Println("No messages in queue..")
-			}
-		}
-	}()
-
-	return ch
 }
